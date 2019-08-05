e115896d1bebbd1788440e1e521bf1af63c5b38e
Ezequiel Zuccon <zombierabbit@users.noreply.github.com>
2019-08-05 14:37:25 -0300

    Fix for Androidx

--- android/src/main/java/com/RNFetchBlob/RNFetchBlob.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlob.java
@@ -6,9 +6,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
-import android.support.v4.content.FileProvider;
 import android.util.SparseArray;
 
+import androidx.core.content.FileProvider;
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.LifecycleEventListener;
